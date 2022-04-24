e8a2306f5e917bfd3214baf1138a48c340adbc58
Teresiah-cpu <terrywambo2018@gmail.com>
2022-04-24 12:52:18 +0300

    add a finction to welcome a user

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
 
+def main():
+    print("Hello Welcome to your password locker site. What is your name?")
+    name = input()
+
+    print(f"Hello {name}. what would you like to do?")
+    print('\n')
