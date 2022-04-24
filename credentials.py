7f28565e71d5952e36cf6b8dd3542d5571b0ed0d
Teresiah-cpu <terrywambo2018@gmail.com>
2022-04-24 13:19:52 +0300

    add a function to create new account

--- credentials.py
+++ credentials.py
@@ -29,3 +29,4 @@ class Credentials:
         '''
 
         Credentials.accounts_list.append(self)
+        
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+from credentials import Credentials
+from user import user
+
+
 
 def main():
     print("Hello Welcome to your password locker site. What is your name?")
@@ -5,3 +9,42 @@ def main():
 
     print(f"Hello {name}. what would you like to do?")
     print('\n')
+
+
+while True:
+        print("Hello. Welcome back to Password Locker!")
+        print('\n')
+        print('Create a login username and a password')
+        print('\n')
+        print('Enter username')
+        username =  input()
+        print('Enter password')
+        user_password = input()
+
+        print('To login in, Enter your username')
+        input_username = input()
+        if (input_username == username):
+            print('Enter your password')
+            input_password = input()
+            if (input_password == user_password):
+                print("Use these short codes : cr - create a new account, di - display accounts, fi -find an account, ex -exit the account list ")
+
+                short_code:any = input().lower()
+
+                if short_code == "ca":
+                    print("New Account")
+                    print("-"*10)
+
+                    print ("Account name ...")
+                    acc_name = input()
+
+                    print("Username ...")
+                    user_name = input()
+
+                    print("Password ...")
+                    user_password = input()
+
+                    save_credential(create_account(acc_name, user_name, user_password)) # create and save new account.
+                    print('\n')
+                    print(f"New {acc_name} Account Created")
+                    print('\n')
