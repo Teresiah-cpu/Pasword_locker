65ae9a6411bc078d54091db7efe3e6f2b498e68b
Teresiah-cpu <terrywambo2018@gmail.com>
2022-04-24 10:11:44 +0300

    add a function to define the variables to be used

---
+++ credentials.py
@@ -0,0 +1,16 @@
+class Credentials:
+    """
+    Class that generates new instances of credentials.
+    """
+
+    accounts_list = [] 
+    
+
+    def __init__(self, acc_name, user_name, user_password):
+        '''
+        method that defines variables to be used
+        '''
+
+        self.acc_name = acc_name
+        self.user_name = user_name
+        self.user_password = user_password
